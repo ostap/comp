8b8b6aa225fec7a6e5811c65a787ffb866dba40b
Ostap Cherkashin <ostap@bandilab.org>
2013-02-10 22:39:44 +0100

    faster (though less accurate) implementation of geospatial distance

--- src/comp/distance.go
+++ src/comp/distance.go
@@ -12,5 +12,8 @@ func Dist(lat1, lon1, lat2, lon2 float64) float64 {
 	lat2 = lat2 * Pi / 180.0
 	lon2 = lon2 * Pi / 180.0
 
-	return EarthRadiusKm * Acos(Sin(lat1)*Sin(lat2)+Cos(lat1)*Cos(lat2)*Cos(lon2-lon1))
+	x := (lon2 - lon1) * Cos((lat1+lat2)/2)
+	y := (lat2 - lat1)
+
+	return Sqrt(x*x+y*y) * EarthRadiusKm
 }
